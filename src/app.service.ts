932acee3d01144e31f4f5f8a352b426d30c5a465
Iago H Nunes <iagohnunes6@gmail.com>
2026-01-30 15:33:05 -0300

    feat: Implementação do prisma como ORM

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getHello() {
+    return await this.prisma.$transaction(async (cursor) => {
+      return cursor.teste.findMany();
+    });
+  }
+
+  
+  async insert() {
+    await this.prisma.$transaction(async (cursor) => {
+      const random = Math.floor(Math.random() * 100);
+      await cursor.teste.create({ data: { descricao: 'Barco '+random } });
+    });
   }
 }
